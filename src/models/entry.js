3d7ac71867471b00bc338b1a33222d10817659be
Juho Härme <juho.harme@gmail.com>
2019-02-12 16:15:42 +0200

    fixed adding entries with existing authors

--- src/models/entry.js
+++ src/models/entry.js
@@ -3,43 +3,39 @@ import Author from './author';
 
 const { ObjectId } = Schema.Types;
 
+const saveEntry = (authorId, textType, response) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const entry = new Entry({ author: authorId, textType });
+  entry.save((entryErr, savedEntry) => {
+    if (entryErr) {
+      response.status(400).send({ error: 'Could not save the entry' });
+    } else {
+      response.status(200).send(savedEntry);
+    }
+  });
+};
+
 const entrySchema = new Schema({
   author: { type: ObjectId, ref: 'Author' },
-  text_type: { type: String, default: '?' },
+  textType: { type: String, default: '?' },
 });
 
 entrySchema.static('addNew', async function addNew(request, response) {
   // Get the parameters from the request body
   const { authorName, textType } = request.body;
-  let authorId;
   // Look for the author in the authors db
-  await Author.findOne({ name: authorName }, (err, existingAuthor) => {
-    if (err) {
-      console.log('error in fetching an existing author');
-    } else if (existingAuthor) {
-      authorId = existingAuthor._id;
-    } else {
-      // if not found, create a new author based on the name
-      const author = new Author({ name: authorName });
-      author.save((authorErr, savedAuthor) => {
-        if (authorErr) {
-          response.status(400).send({ error: 'Could not save the author' });
-        } else {
-          authorId = savedAuthor._id;
-        }
-      });
-    }
-  });
-  if (authorId) {
-    // eslint-disable-next-line no-underscore-dangle
-    const entry = new Entry({ author: authorId, textType });
-    entry.save((entryErr, savedEntry) => {
-      if (entryErr) {
-        response.status(400).send({ error: 'Could not save the entry' });
-      } else {
-        response.status(200).send(savedEntry);
+  const oldauthor = await Author.findOne({ name: authorName });
+  if (!oldauthor) {
+    // if not found, create a new author based on the name
+    const author = new Author({ name: authorName });
+    await author.save((authorErr, savedAuthor) => {
+      if (authorErr) {
+        response.status(400).send({ error: 'Could not save the author' });
       }
+      saveEntry(savedAuthor._id, textType, response);
     });
+  } else {
+    saveEntry(oldauthor._id, textType, response);
   }
 });
 
