ef2d45b956400c250744a6f940a48e780906c744
Juho Härme <juho.harme@gmail.com>
2019-02-12 09:26:29 +0200

    server test ok

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ const app = express();
 app.get('/test', (request, response) => {
   response.status(200).send('test ok');
 });
+
+app.listen(3000, 'localhost', () => {
+  // eslint-disable-next-line no-console
+  console.log('listening..');
+});
