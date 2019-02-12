b32384b5c73104fecf7044f2f64ac50348049839
Juho Härme <juho.harme@gmail.com>
2019-02-12 14:38:49 +0200

    using static method for saving entries

--- src/index.js
+++ src/index.js
@@ -12,37 +12,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 app.post('/entry', (request, response) => {
-  // Get the parameters from the request body
-  const { authorName, textType } = request.body;
-  let authorId;
-  // Look for the author in the authors db
-  Author.findOne({ name: authorName }, (err, existingAuthor) => {
-    if (err) {
-      console.log('error in fetching an existing author');
-    } else if (existingAuthor) {
-      authorId = existingAuthor._id;
-    } else {
-      // if not found, create a new author based on the name
-      author.save((authorErr, savedAuthor) => {
-        if (authorErr) {
-          response.status(400).send({ error: 'Could not save the author' });
-        } else {
-          authorId = savedAuthor._id;
-        }
-      });
-    }
-    if (authorId) {
-      // eslint-disable-next-line no-underscore-dangle
-      const entry = new Entry({ author: authorId, textType });
-      entry.save((entryErr, savedEntry) => {
-        if (entryErr) {
-          response.status(400).send({ error: 'Could not save the entry' });
-        } else {
-          response.status(200).send(savedEntry);
-        }
-      });
-    }
-  });
+  Entry.addNew(request, response);
 });
 
 app.post('/author', (request, response) => {
--- src/models/entry.js
+++ src/models/entry.js
@@ -8,6 +8,41 @@ const entrySchema = new Schema({
   text_type: { type: String, default: '?' },
 });
 
+entrySchema.static('addNew', async function addNew(request, response) {
+  // Get the parameters from the request body
+  const { authorName, textType } = request.body;
+  let authorId;
+  // Look for the author in the authors db
+  await Author.findOne({ name: authorName }, (err, existingAuthor) => {
+    if (err) {
+      console.log('error in fetching an existing author');
+    } else if (existingAuthor) {
+      authorId = existingAuthor._id;
+    } else {
+      // if not found, create a new author based on the name
+      const author = new Author({ name: authorName });
+      author.save((authorErr, savedAuthor) => {
+        if (authorErr) {
+          response.status(400).send({ error: 'Could not save the author' });
+        } else {
+          authorId = savedAuthor._id;
+        }
+      });
+    }
+  });
+  if (authorId) {
+    // eslint-disable-next-line no-underscore-dangle
+    const entry = new Entry({ author: authorId, textType });
+    entry.save((entryErr, savedEntry) => {
+      if (entryErr) {
+        response.status(400).send({ error: 'Could not save the entry' });
+      } else {
+        response.status(200).send(savedEntry);
+      }
+    });
+  }
+});
+
 const Entry = model('Entry', entrySchema);
 
 export default Entry;
